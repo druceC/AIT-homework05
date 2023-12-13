daf3bef57959ab40b5be30ad9f734de75b93924e
Druce Chua <89497828+druceC@users.noreply.github.com>
2023-12-13 10:29:25 -0500

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,20 +1,3 @@
-// import express from 'express';
-// const app = express();
-
-// to parse json bodies, uncomment the following line:
-// app.use(express.json())
-
-// import url from 'url';
-// import path from 'path';
-// const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.listen(3000);
-// 
-
-// app.mjs
-
 import express from 'express';
 import url from 'url';
 import path from 'path';
@@ -24,19 +7,6 @@ const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
-
-
-// app.post('/submit-form', (req, res) => {
-//   // Handle the form submission logic here
-//   // You can send a response or redirect as needed
-//   res.send('Form submitted successfully!');
-//   console.log('Form submitted successfully!');
-// });
-
-// app.addEventListener('click',function(evt){
-//     console.log('button clicked!');
-//     evt.preventDefault();
-// });
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
